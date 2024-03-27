90b1d17c3ee9fa6a76464a88fea777ce7039415e
Vishv <vishvsalvi10@gmail.com>
2024-03-27 20:46:02 +0530

    Debugging output changes

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,8 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export default function page() {
+export default function Page() {
+
   useEffect(() => {
     AOS.init({ duration: 3000 });
   }, []);
--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <h2 className="text-3xl">
         There was a problem with your request
     </h2>
-    <p>We couldn't find the page you were looking for. </p>
+    <p>We couldn`&apos;`t find the page you were looking for. </p>
     Return to <Link href='/' className='text-blue-600'>Home page</Link>
 </div>
   )
